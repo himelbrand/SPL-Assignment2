5424a36fcf8aad242632f109eb58b791f85055fd
himelbrand <himelbrand@gmail.com>
2016-12-11 20:58:15 +0200

    VersionMonitorTest

--- src/test/java/bgu/spl/a2/VersionMonitorTest.java
+++ src/test/java/bgu/spl/a2/VersionMonitorTest.java
@@ -1,5 +1,7 @@
 package bgu.spl.a2;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,6 +10,16 @@ import static org.junit.Assert.*;
  * Created by himelbrand on 12/11/16.
  */
 public class VersionMonitorTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
     @Test
     public void getVersion() throws Exception {
 
