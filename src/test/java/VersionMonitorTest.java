8aa05386ed88987403a09d8ea798637cc597cf60
himelbrand <himelbrand@gmail.com>
2016-12-11 19:47:28 +0200

    VersionMonitorTest

---
+++ src/test/java/VersionMonitorTest.java
@@ -0,0 +1,36 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by himelbrand on 12/11/16.
+ */
+public class VersionMonitorTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getVersion() throws Exception {
+
+    }
+
+    @Test
+    public void inc() throws Exception {
+
+    }
+
+    @Test
+    public void await() throws Exception {
+
+    }
+
+}
