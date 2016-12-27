8066cbdd863f5ecf273383347f2d9985225d9721
himelbrand <himelbrand@gmail.com>
2016-12-27 20:11:25 +0200

    update

---
+++ src/main/java/bgu/spl/a2/sim/tools/NextPrimeHammer.java
@@ -0,0 +1,18 @@
+package bgu.spl.a2.sim.tools;
+
+import bgu.spl.a2.sim.Product;
+
+/**
+ * Created by himelbrand on 12/27/16.
+ */
+public class NextPrimeHammer implements Tool {
+    @Override
+    public String getType() {
+        return null;
+    }
+
+    @Override
+    public long useOn(Product p) {
+        return 0;
+    }
+}
