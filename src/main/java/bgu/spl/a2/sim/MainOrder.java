ec2ddeab96426a17b35808765b719c4a7b9be250
himelbrand <himelbrand@gmail.com>
2016-12-27 23:56:32 +0200

    update

--- src/main/java/bgu/spl/a2/sim/MainOrder.java
+++ src/main/java/bgu/spl/a2/sim/MainOrder.java
@@ -4,62 +4,38 @@ package bgu.spl.a2.sim;
  * Created by himelbrand on 12/27/16.
  */
 public class MainOrder {
-    class Waves
-    {
+    class Waves{
         String product;
-
         String startId;
-
         String qty;
-
-
-
-        @Override
-        public String toString()
+        public Waves(){}
+         public String toString()
         {
             return "ClassPojo [product = "+product+", startId = "+startId+", qty = "+qty+"]";
         }
     }
-    class Plans
-    {
+    class Plans{
         String product;
-
         String[] tools;
-
         String[] parts;
-
-
-
-
-
-        @Override
         public String toString()
         {
             return "ClassPojo [product = "+product+", tools = "+tools+", parts = "+parts+"]";
         }
     }
-    class Tools
-    {
+    class Tools{
         String tool;
-
         String qty;
-
-
-        @Override
         public String toString()
         {
             return "ClassPojo [tool = "+tool+", qty = "+qty+"]";
         }
     }
+    //Main Order Fields and toString
     Plans[] plans;
-
     Tools[] tools;
-
     String threads;
-
     Waves[][] waves;
-    
-    @Override
     public String toString()
     {
         return "ClassPojo [plans = "+plans+", tools = "+tools+", threads = "+threads+", waves = "+waves+"]";
