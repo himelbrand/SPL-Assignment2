97999909f66729c86d027c2bf5b5d3f3516562a7
Shahar <nussbaus@post.bgu.ac.il>
2016-12-27 21:35:57 +0200

    shahar work111

---
+++ src/main/java/bgu/spl/a2/sim/tasks/ManufatoringTask.java
@@ -0,0 +1,15 @@
+package bgu.spl.a2.sim.tasks;
+
+import bgu.spl.a2.Task;
+import bgu.spl.a2.sim.Product;
+
+/**
+ * Created by Shahar on 27/12/2016.
+ */
+public class ManufatoringTask extends Task<Product> {
+
+    @Override
+    protected void start() {
+
+    }
+}
